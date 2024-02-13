864d0b2723c2e221637f5013b0e265288a5b428a
Diego <81425000+Diego582@users.noreply.github.com>
2024-02-13 17:54:08 -0300

    crudClient (#2)

--- routes/customer.js
+++ routes/customer.js
@@ -1,23 +1,27 @@
 import express from "express";
-import create from '../controllers/customers/create.js';
-import destroy from '../controllers/customers/destroy.js';
-import read from '../controllers/customers/read.js';
-import readOne from '../controllers/customers/readOne.js';
-import update from '../controllers/customers/destroy.js'
+import create from "../controllers/customers/create.js";
+import destroy from "../controllers/customers/destroy.js";
+import read from "../controllers/customers/read.js";
+import readOne from "../controllers/customers/readOne.js";
+import update from "../controllers/customers/destroy.js";
+
+import isValidCustomer from "../middlewares/isValidCustomer.js";
+import validator from "../middlewares/validator.js";
+import customerSchema from "../schema/customerSchema.js";
 
 const router = express.Router();
 
 /* GET users listing. */
-router.post('/', create)
+router.post("/", isValidCustomer, validator(customerSchema), create);
 
 //READ
-router.get('/', read)
-router.get('/:id', readOne)
+router.get("/", read);
+router.get("/:id", readOne);
 
 //UPDATE
-router.put('/:id',update)
+router.put("/:id", update);
 
 //DESTROY
-router.delete('/:id', destroy)
+router.delete("/:id", destroy);
 
 export default router;
