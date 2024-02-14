959a09788b19bd12dcea4b56849a38543c689a7d
Diego <81425000+Diego582@users.noreply.github.com>
2024-02-13 22:05:15 -0300

    clientSearch (#4)

--- controllers/customers/read.js
+++ controllers/customers/read.js
@@ -1,10 +1,11 @@
 import Customer from "../../models/Customer.js";
 
-
 export default async (req, res, next) => {
   try {
     let queries = {};
-    
+    if (req.query.lastName) {
+      queries.lastName = new RegExp(req.query.lastName, "i");
+    }
     const allCustomer = await Customer.find(
       queries,
       "-__v -createdAt -updatedAt"
