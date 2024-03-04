6fb63993a668aaba245d95d0851ff81e0ef369ad
Diego <penaomardiego@gmail.com>
2024-03-03 22:47:14 -0300

    cargaProducto

--- controllers/productBase/read.js
+++ controllers/productBase/read.js
@@ -3,16 +3,12 @@ import ProductBase from "../../models/ProductBase.js";
 export default async (req, res, next) => {
   try {
     let queries = {};
-    if (req.query.descripcion) {
-      queries.descripcion = new RegExp(req.query.descripcion, "i");
-    }
     if (req.query.codigoBarras) {
       queries.codigoBarras = new RegExp(req.query.codigoBarras, "i");
     }
-    const allProductBase = await ProductBase.find(
-      queries,
-      "-__v -createdAt -updatedAt"
-    ).select("");
+    const allProductBase = await ProductBase.findOne({
+      codigoBarras: req.query.codigoBarras,
+    }).select("");
 
     return res.status(200).json({
       success: true,
