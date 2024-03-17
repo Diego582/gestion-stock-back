c06631e01b4b0133dee295bc839b99e99256430f
Diego <81425000+Diego582@users.noreply.github.com>
2024-03-16 23:12:49 -0300

    productcompletado (#13)

--- controllers/productBase/create.js
+++ controllers/productBase/create.js
@@ -2,6 +2,16 @@ import ProductBase from "../../models/ProductBase.js";
 
 export default async (req, res, next) => {
   try {
+
+    if (req.isValidProductBase) {
+      return res.status(409).json({
+        success: false,
+        message: "duplicate product en BBDD - " + req.body.codigoBarras,
+        response: null,
+      });
+    }
+
+
     let newProduct = await ProductBase.create(req.body);
     return res.status(201).json({
       success: true,
