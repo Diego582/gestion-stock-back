a5439414dbc25e22863356e777f0a8e157907a11
Diego <81425000+Diego582@users.noreply.github.com>
2024-07-29 23:51:44 -0300

    Print (#25)

---
+++ controllers/check/readLast.js
@@ -0,0 +1,17 @@
+import Check from "../../models/Check.js";
+
+export default async (req, res, next) => {
+  try {
+
+    let oneCheck = await Check.findOne({}, {}, { sort: { createdAt: -1 } })
+
+
+    return res.status(200).json({
+      success: true,
+      message: "Check found",
+      response: oneCheck,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- controllers/check/readOne.js
+++ controllers/check/readOne.js
@@ -3,8 +3,14 @@ import Check from "../../models/Check.js";
 export default async (req, res, next) => {
   try {
 
-    let oneCheck = await Check.findOne({}, {}, { sort: { createdAt: -1 } })
-
+    let oneCheck = await Check.findOne({ _id: req.params._id })
+      .populate('client_id')
+      .populate({
+        path: 'products_id',
+        populate: {
+          path: 'id_product',  // Cambia 'category' por el campo que deseas poblar dentro de 'products_id'
+        }
+      });
 
     return res.status(200).json({
       success: true,
